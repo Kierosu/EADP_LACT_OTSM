30a57bbec964c2c4679ea5e57c824aeff8800343
Shizuko <32918698+AhmadShafie@users.noreply.github.com>
2018-12-19 18:59:54 +0800

    get tdAspect from database and add into LabelAspects

--- Website/Homepage.aspx.cs
+++ Website/Homepage.aspx.cs
@@ -28,17 +28,18 @@ public partial class _Default : System.Web.UI.Page
     protected void ButtonBlog_Click(object sender, EventArgs e)
     {
         MultiViewTrip.ActiveViewIndex = 1;
-    }       
+    }
 
     protected void ButtonStats_Click(object sender, EventArgs e)
     {
+        string aspectsList = "";
         MultiViewTrip.ActiveViewIndex = 2;
         Series series = Chart1.Series["Series1"];
         string DBConnect = ConfigurationManager.ConnectionStrings["ConnStr"].ConnectionString;
         SqlConnection myConn = new SqlConnection(DBConnect);
 
         StringBuilder sqlCommand = new StringBuilder();
-        sqlCommand.AppendLine("Select tdRating, tdReview from TableStats");
+        sqlCommand.AppendLine("Select tdRating, tdReview,tdAspect from TableStats");
         SqlDataAdapter da = new SqlDataAdapter(sqlCommand.ToString(), myConn);
 
         DataSet ds = new DataSet();
@@ -51,11 +52,15 @@ public partial class _Default : System.Web.UI.Page
             DataRow row = ds.Tables["TableStats"].Rows[i];
             LabelComments.Text += "Student " + (i + 1) + ": " + row["tdRating"].ToString() + "/5. Comment: " + row["tdReview"] + "<br/>";
             series.Points.AddXY("Student " + (i + 1).ToString(), row["tdRating"]);
+            aspectsList += row["tdAspect"];
+            string[] aspects = aspectsList.Split(',');
+            for (int j = 0; j < aspects.Length; j++)
+            {
+                LabelAspects.Text += "Aspect no." + (j + 1) + ": " + aspects[j] + "<br/>";
+
+            }
+            ButtonStats.Enabled = false;
+            ButtonDetails.Enabled = true;
         }
-        ButtonStats.Enabled = false;
-        ButtonDetails.Enabled = true;
     }
-
-
-    
 }
