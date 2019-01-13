f0d4bf413d7a8bdd7d91f95c30175f62a9480142
Lee Mun Hong <170537U@mymail.nyp.edu.sg>
2019-01-13 23:06:42 +0800

    Putting into blog and extracting from blog

--- Website/BlogPost.aspx
+++ Website/BlogPost.aspx
@@ -3,18 +3,25 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <br /> <br /> <br /> <br /> <br /> 
     <asp:Label ID="Label4" runat="server" Text="Blog Creation" Font-Size="30px"></asp:Label>
     <br />    
+    <div class="form-group">
     <asp:Label ID="Label1" runat="server" Text="Blog Title : "></asp:Label>
-    <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+    <asp:TextBox ID="tbBlogTitle" runat="server"></asp:TextBox>
+    </div>
     <br />
+    <div class="form-group">
     <asp:Label ID="Label2" runat="server" Text="Upload Image : "></asp:Label>
-    <asp:ImageButton ID="ImageButton1" runat="server" Width="26px" />
+    <asp:FileUpload ID="FileUpload1" runat="server" />
+    </div>
     <br />
+    <div class="form-group">
     <asp:Label ID="Label3" runat="server" Text="Description : "></asp:Label>
     <br />
-    <asp:TextBox ID="TextBox2" runat="server" Height="108px" Width="205px"></asp:TextBox>
+    <asp:TextBox ID="tbBlogDetails" runat="server" Height="108px" Width="205px"></asp:TextBox>
     <br />
-    <asp:Button ID="Button1" runat="server" Text="Confirm" />
+    </div>
+    <asp:Button ID="ButtonCfm" runat="server" Text="Confirm" OnClick="Button1_Click" />
 </asp:Content>
 
---
+++ Website/BlogView.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class BlogView : System.Web.UI.Page
+{
+    string DBConnect = ConfigurationManager.ConnectionStrings["ConnStr"].ConnectionString;
+    
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+        SqlDataAdapter da;
+        DataSet ds = new DataSet();
+
+        //Create Adapter
+        //WRITE SQL Statement to retrieve all columns from Customer by customer Id using query parameter
+        StringBuilder sqlCommand = new StringBuilder();
+        sqlCommand.AppendLine("Select * from blogs where");
+        sqlCommand.AppendLine("TripId = @paraId");
+        //***TO Simulate system error  *****
+        // change custId in where clause to custId1 or 
+        // change connection string in web config to a wrong file name  
+
+        SqlConnection myConn = new SqlConnection(DBConnect);
+        da = new SqlDataAdapter(sqlCommand.ToString(), myConn);
+        da.SelectCommand.Parameters.AddWithValue("paraId", 1);
+        // fill dataset
+        da.Fill(ds, "tripblogTable");
+        int rec_cnt = ds.Tables["tripblogTable"].Rows.Count;
+        if (rec_cnt > 0)
+        {
+            DataRow row = ds.Tables["tripblogTable"].Rows[0];  // Sql command returns only one record
+            lblBlogTitle.Text = row["BlogTitle"].ToString();
+            byte[] imageData = (Byte[])row["BlogImage"];
+            string img = Convert.ToBase64String(imageData, 0, imageData.Length);
+            Image1.ImageUrl = "data:image/png;base64," + img;
+
+            lblBlogDetails.Text = row["BlogDetails"].ToString();
+        }
+        else
+        {
+
+        }
+    }
+}
