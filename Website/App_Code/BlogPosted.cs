92d48f9d00786bfa54692ff03cb3bdb0b76e6407
Lee Mun Hong <170537U@mymail.nyp.edu.sg>
2019-01-16 20:17:59 +0800

    Returning deleted stuff part 2

---
+++ Website/App_Code/BlogPosted.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for BlogPosted
+/// </summary>
+public class BlogPosted
+{
+    public string BlogTitle { get; set; }
+    public string BlogImage { get; set; }
+    public string BlogDetails { get; set; }
+    public BlogPosted()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+}
