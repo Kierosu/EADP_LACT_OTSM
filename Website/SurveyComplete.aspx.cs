649df69a8655581c69c4e86f69f07e6fd2663e51
Shizuko <32918698+AhmadShafie@users.noreply.github.com>
2019-01-19 10:06:08 +0800

    separate class

--- Website/SurveyComplete.aspx.cs
+++ Website/SurveyComplete.aspx.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +11,6 @@ public partial class _Default : System.Web.UI.Page
     {
 
     }
-
     protected void ButtonHome_Click(object sender, EventArgs e)
     {
         Response.Redirect("Homepage.aspx");
